7829cf1bd9fb29946f276b9590765efe49bc2d5a
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-11-04 09:11:11 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    const user = {
+        name,
+        email
+    }
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(user)
+    }).then(response => response.json())
+    .then(result => {
+        const newUserId = result.id
+        console.log(newUserId);
+        document.querySelector('body').innerText = newUserId
+    }).catch(error => console.log(error))
+}
+submitData("Darbi", "darbi@darbi.com")
